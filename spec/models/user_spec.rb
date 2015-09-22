a1d9f2f20ee2fa0283590293d8b225744e4049c3
Fermin Carranza <fcarranza@watscoventures.com>
2015-09-22 14:51:32 -0400

    Shoulda matchers for email, password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,9 @@ RSpec.describe User, type: :model do
     expect(@user).to respond_to(:password)
     expect(@user).to respond_to(:password_confirmation)
   end
+
+  it { expect(@user).to validate_presence_of(:email) }
+  it { expect(subject).to validate_uniqueness_of(:email) }
+  it { expect(subject).to validate_confirmation_of(:password) }
+  it { expect(subject).to allow_value('example@domain.com').for(:email) }
 end
